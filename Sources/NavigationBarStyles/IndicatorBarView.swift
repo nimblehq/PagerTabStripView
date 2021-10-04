dd08c6cf0d18427c46a134f731b3f50410a8fdd7
MarkG <markgravity.in@gmail.com>
2021-10-04 14:53:59 +0700

    Fix complier error

--- Sources/NavigationBarStyles/IndicatorBarView.swift
+++ Sources/NavigationBarStyles/IndicatorBarView.swift
@@ -14,18 +14,26 @@ internal struct IndicatorBarView: View {
 
     var body: some View {
         HStack {
-            let totalItemWidth = (settings.width - (style.tabItemSpacing * CGFloat(dataStore.itemsCount - 1)))
-            let navBarItemWidth = totalItemWidth / CGFloat(dataStore.itemsCount)
-            if let width = navBarItemWidth, width > 0, width <= settings.width {
-                let x = -settings.contentOffset / CGFloat(dataStore.itemsCount) + width / 2
+            content
+        }
+        .frame(height: style.indicatorBarHeight)
+    }
+
+    var content: some View {
+        let totalItemWidth: CGFloat = (settings.width - (style.tabItemSpacing * CGFloat(dataStore.itemsCount - 1)))
+        let navBarItemWidth: CGFloat = totalItemWidth / CGFloat(dataStore.itemsCount)
+        return Group {
+            if let width: CGFloat = navBarItemWidth, width > 0, width <= settings.width,
+               let x: CGFloat = -settings.contentOffset / CGFloat(dataStore.itemsCount) + width / 2{
                 Rectangle()
                     .fill(style.indicatorBarColor)
                     .animation(.default)
                     .frame(width: width)
                     .position(x: x, y: 0)
+            } else {
+                EmptyView()
             }
         }
-        .frame(height: style.indicatorBarHeight)
     }
 
     @Environment(\.pagerStyle) var style: PagerStyle
